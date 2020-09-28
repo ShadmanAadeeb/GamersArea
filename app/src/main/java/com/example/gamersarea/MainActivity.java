90de8975dd2f7204fb1acd73c738fca47e479e59
Aadeeb <aadeeb1896@gmail.com>
2020-09-28 15:34:01 +0600

    Sign Up Activity Layout Created

--- app/src/main/java/com/example/gamersarea/MainActivity.java
+++ app/src/main/java/com/example/gamersarea/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.gamersarea;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button shortcutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void linkAllButtons(){
+        shortcutButton=findViewById(R.id.shortcutButton);
+    }
+
+    void setListenersToButtons(){
+        shortcutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
